bdb2ec162a9abbb545e13c1f74f87a628827e3e4
josandreu <jose.andreu.r@gmail.com>
2019-03-19 21:15:23 +0100

    Añadido código inicial

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*
+const titulo1 = document.getElementById('titulo1');
+
+titulo1.style.color = "lightgray";
+
+function cambiarColor() {
+    titulo1.className = "text-light text-center";
+    // titulo1.style.background = "steelblue";
+}
+
+titulo1.addEventListener("click", cambiarColor);
+*/
+
+// (1) DEFINICIÓN DE VARIABLES
+const article = document.getElementById('article');
+const quantity = document.querySelector('#quantity');
+const priority = document.querySelector('#priority');
+const addButton = document.querySelector('#addButton');
+
+// (2) DEFINICIÓN DE FUNCIONES
+
+// (3) DEFINICIÓN DE EVENTOS
